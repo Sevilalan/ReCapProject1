dc9f69ef43117af5bea611219fd7e72f98f023f3
Sevilalan <61330327+Sevilalan@users.noreply.github.com>
2021-02-03 17:38:53 +0300

    Add project files.

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public interface ICarService
+    {
+        List<Car> GetAll();//tüm ürünleri listeler
+
+
+    }
+}
---
+++ Entities/Concrate/Car.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrate
+{
+    //Id, BrandId, ColorId, ModelYear, DailyPrice, Description
+    public class Car :IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public string ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
